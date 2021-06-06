7e44bc618f358fc97e266202d0b2ab1361677eb2
kshired <shiroed1211@naver.com>
2021-06-07 00:51:33 +0900

    env setting

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ server.use((_, res) => {
 
 const io = socketIO(compoundedServer, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.CLIENT_URL,
     methods: ['GET', 'POST'],
   },
 });
